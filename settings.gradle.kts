5c721010e3e03703bf2909148465c0aec7e6dea3
liuwenlong6 <liuwenlong6@xiaomi.com>
2026-09-17 19:43:55 +0800

    build: 添加阿里云镜像源加速构建

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,10 @@ rootProject.name = "ALLINONE"
 
 pluginManagement {
     repositories {
+        maven("https://maven.aliyun.com/repository/gradle-plugin")
+        maven("https://maven.aliyun.com/repository/google")
+        maven("https://maven.aliyun.com/repository/central")
+        maven("https://maven.aliyun.com/repository/public")
         google()
         mavenCentral()
         gradlePluginPortal()
@@ -10,6 +14,9 @@ pluginManagement {
 
 dependencyResolutionManagement {
     repositories {
+        maven("https://maven.aliyun.com/repository/google")
+        maven("https://maven.aliyun.com/repository/central")
+        maven("https://maven.aliyun.com/repository/public")
         google()
         mavenCentral()
     }
